7ff9f112227e23fce95b74765ee8317f931fdefb
jackychanfox <fox0905@outlook.com>
2018-04-06 06:18:44 -0700

    remove "@override"

--- android/src/main/java/com/rome2rio/android/reactnativetouchthroughview/TouchThroughViewPackage.java
+++ android/src/main/java/com/rome2rio/android/reactnativetouchthroughview/TouchThroughViewPackage.java
@@ -19,17 +19,17 @@ public class TouchThroughViewPackage implements ReactPackage {
     public TouchThroughViewPackage() {
     }
 
-    @Override
+    
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
 
-    @Override
+    
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
+    
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         TouchThroughViewManager touchThroughViewManager = new TouchThroughViewManager();
         TouchThroughWrapperManager touchThroughWrapperManager = new TouchThroughWrapperManager();
