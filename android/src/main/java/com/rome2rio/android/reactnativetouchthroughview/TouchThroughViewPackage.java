cc92d11d811780023ccf308d3857fa80b72cc0e6
Quan Pham <doraemonfanclub@gmail.com>
2018-07-11 11:34:45 +0700

    Upgrade Android native build tools and dependencies

--- android/src/main/java/com/rome2rio/android/reactnativetouchthroughview/TouchThroughViewPackage.java
+++ android/src/main/java/com/rome2rio/android/reactnativetouchthroughview/TouchThroughViewPackage.java
@@ -3,7 +3,6 @@ package com.rome2rio.android.reactnativetouchthroughview;
 import android.app.Activity;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -21,11 +20,6 @@ public class TouchThroughViewPackage implements ReactPackage {
 
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
